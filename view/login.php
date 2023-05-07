209a5fb06d21430fc4c8262a10fe2b3816c7ecb6
fernandomesa2000 <juan.bedoyame@amigo.edu.co>
2023-05-07 18:15:39 -0500

    cambiecito

---
+++ view/login.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Iniciar Sesión</h1>
+    <form action="">
+        <input type="mail" name= "correo" placeholder= "Correo">
+        <input type="password" name= "contrasena" placeholder= "Contraseña">
+        <input type="submit" value="Enviar">
+        <a href="index.php">Registrarse</a>
+    </form>
+</body>
+</html>
