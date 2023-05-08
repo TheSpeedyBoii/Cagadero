cae76ee2fb88b2b476c2bba5d3b394d5a593416c
TheSpeedyBoii <agudelotoxcino@gmail.com>
2023-05-07 19:13:14 -0500

    Hola mi amor

--- view/index.php
+++ view/index.php
@@ -15,24 +15,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="../view/index.css">
 </head>
 <body>
+    <img src="../view/assets/images/logo.png" alt="">
     <div class="container">
-        <form action="index.php?action=registrar" method="post">
-            <div class="mb-3">
-                <label for="nombre" class="form-label">Nombre</label>
-                <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre">
-            </div>
-            <div class="mb-3">
-                <label for="correo" class="form-label">Correo electrónico</label>
-                <input type="email" class="form-control" name="correo" id="correo" placeholder="Correo electrónico">
-            </div>
-            <div class="mb-3">
-                <label for="contrasena" class="form-label">Contraseña</label>
-                <input type="password" class="form-control" name="contrasena" id="contrasena" placeholder="Contraseña">
-            </div>
-            <button type="submit" class="btn btn-primary">Registrarse</button>
-        </form>
+        <div class="form-container">
+            <form action="index.php?action=registrar" method="post">
+                <div class="mb-3">
+                    <label for="nombre" class="form-label">Nombre</label>
+                    <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Ingrese su nombre">
+                </div>
+                <div class="mb-3">
+                    <label for="correo" class="form-label">Correo electrónico</label>
+                    <input type="email" class="form-control" name="correo" id="correo" placeholder="Ingrese su correo">
+                </div>
+                <div class="mb-3">
+                    <label for="contrasena" class="form-label">Contraseña</label>
+                    <input type="password" class="form-control" name="contrasena" id="contrasena" placeholder="Ingrese su contraseña">
+                </div>
+                <button type="submit" class="btn btn-primary">Registrarse</button>
+            </form>
+        </div>
     </div>
 </body>
 </html>
+
