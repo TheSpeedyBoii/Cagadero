2fbff5a63e522ae965d6f166ce1a3dcbc934c043
TheSpeedyBoii <agudelotoxcino@gmail.com>
2023-05-11 21:58:45 -0500

    Welcome

--- view/register.php
+++ view/register.php
@@ -15,10 +15,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="../view/register.css">
+    <link rel="stylesheet" href="styles/register.css">
 </head>
 <body>
-    <img src="../view/assets/images/logo.png" alt="">
+    <img src="images/register/logo.png" alt="">
     <div class="container">
         <div class="form-container">
             <form action="register.php?action=registrar" method="post">
--- view/welcome.php
+++ view/welcome.php
@@ -6,12 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Parallel</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jgthms/minireset.css@master/minireset.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="styles/welcomeStyles.css">
 </head>
 <body>
     <header>
+        <div class="background">
+            
+        </div>
         <nav>
-            <img src="images/landing/land.png" alt="">
+            <img src="images/landing/Logo.png" alt="">
             <ul>
                 <li>Destinos</li>
                 <li>Tarifas</li>
