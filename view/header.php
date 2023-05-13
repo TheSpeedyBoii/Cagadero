5ae80bb13b027fa124c3ba2fdc024d69d1cbe754
fernandomesa2000 <juan.bedoyame@amigo.edu.co>
2023-05-12 19:41:31 -0500

    Landing

---
+++ view/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Parallel</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/jgthms/minireset.css@master/minireset.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/header.css">
+</head>
+<header>
+        <nav>
+            <img src="images/register/Logo.png" alt="">
+            <ul>
+                <li>Destinos</li>
+                <li>Tarifas</li>
+                <li>Contactanos</li>
+                <li>Salir</li>
+            </ul>
+        </nav>
+</header>
+</body>
+</html>
