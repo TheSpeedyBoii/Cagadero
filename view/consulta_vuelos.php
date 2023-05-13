a7e26a5093b6a21e54d392200eaf212782e15d03
TheSpeedyBoii <agudelotoxcino@gmail.com>
2023-05-13 09:43:46 -0500

    Consultas

---
+++ view/consulta_vuelos.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vuelos Disponibles</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+    <?php
+    require_once('header.php')
+    ?>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Codigo Vuelo</th>
+      <th scope="col">Ruta</th>
+      <th scope="col">Fecha Salida</th>
+      <th scope="col">Fecha Llegada</th>
+      <th scope="col">Estado</th>
+      <th scope="col">Asientos Disponibles</th>
+      <th scope="col">Precio</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+</body>
+</html>
